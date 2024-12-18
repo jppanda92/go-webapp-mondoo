49631652e42004414a96ab5c99dacbe6bb448b05
jppanda92 <jppanda.92@gmail.com>
2024-12-18 11:09:45 +0530

    Initial Go HTTP server implementation

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Hello from Jyoti Prakash Panda!")
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    port := ":8080"
+    fmt.Printf("Server listening on %s...\n", port)
+    if err := http.ListenAndServe(port, nil); err != nil {
+        fmt.Printf("Error starting server: %s\n", err)
+    }
+}
